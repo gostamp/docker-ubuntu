3a63e0a3950e0b951faa13088d232ad53faf198b
Skip Baney <twelvelabs@gmail.com>
2023-03-04 02:49:42 +0000

    chore: just calculate version on the host

--- bin/version.sh
+++ bin/version.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
-CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
-if [[ "${CURRENT_BRANCH}" == "main" ]]; then
-    echo "Current branch:  $CURRENT_BRANCH"
-else
-    echo "🔴 Branch '$CURRENT_BRANCH' is not a release branch - exiting."
-    exit 0
-fi
-
 # cspell: words koozz
 if gh extension list | grep -q "koozz/gh-semver"; then
-    gh extension upgrade koozz/gh-semver >/dev/null
+    gh extension upgrade koozz/gh-semver &>/dev/null
 else
-    gh extension install koozz/gh-semver >/dev/null
+    gh extension install koozz/gh-semver &>/dev/null
 fi
 
+CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 CURRENT_VERSION=$(gh release view --json tagName --jq .tagName)
 NEXT_VERSION=$(gh semver)
 
-echo "Current version: $CURRENT_VERSION"
+echo "Current branch:  $CURRENT_BRANCH" 1>&2
+echo "Current version: $CURRENT_VERSION" 1>&2
 if [[ "$CURRENT_VERSION" == "$NEXT_VERSION" ]]; then
-    echo "🔴 No commits since $CURRENT_VERSION that would trigger a new release - exiting."
+    echo "🔴 No commits since $CURRENT_VERSION that would trigger a new release - exiting." 1>&2
     exit 0
 fi
-echo "Next version:    $NEXT_VERSION"
-
-# Set the `next_version` output parameter on the workflow step.
-set -x
-echo "next_version=$NEXT_VERSION" >>"${GITHUB_OUTPUT:-/dev/null}"
-set +x
+echo "Next version:    $NEXT_VERSION" 1>&2
