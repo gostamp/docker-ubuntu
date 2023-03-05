a4defed783289d8c16b0e8283e02fcfef7de3aaa
Skip Baney <twelvelabs@gmail.com>
2023-03-04 22:28:38 -0600

    feat: sign images with cosign (#9)
    * feat: sign images with cosign
    
    * chore: pin GH action

--- bin/version.sh
+++ bin/version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
-# cspell: words koozz oneline
+# cspell: words koozz
 if ! gh extension list | grep -q "koozz/gh-semver"; then
     gh extension install koozz/gh-semver &>/dev/null
 fi
@@ -10,7 +10,7 @@ git fetch --all --tags 1>&2
 CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 CURRENT_VERSION=$(gh release view --json tagName --jq .tagName)
 NEXT_VERSION=$(gh semver)
-COMMITS=$(git log --oneline --no-decorate "$CURRENT_VERSION...HEAD")
+COMMITS=$(git log --color=always --format=" - %C(yellow)%h%Creset %s" "$CURRENT_VERSION...HEAD") # cspell: disable-line
 
 echo ""
 echo "Current branch:  $CURRENT_BRANCH"
